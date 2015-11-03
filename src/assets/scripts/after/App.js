6ed48aebc19b59ae93a2686b5b5b3adc95b79784
Floater <floater@Nerdery-OSX-2.local>
2015-11-02 22:21:01 -0600

    Lazy loaded fonts.

--- src/assets/scripts/after/App.js
+++ src/assets/scripts/after/App.js
@@ -31,7 +31,27 @@ define(function(require, exports, module) { // jshint ignore:line
             dots: true,
             speed: 500
         });
+
+        $(document).ready(function() {
+            this.loadCustomFonts();
+        }.bind(this));
     };
+
+    proto.loadCustomFonts = function() {
+        window.WebFontConfig = {
+            google: { families: [ 'Roboto:400,100italic,100,300,300italic,400italic,500,500italic,700,700italic,900,900italic:latin' ] }
+        };
+
+        (function() {
+            var wf = document.createElement('script');
+            wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+            wf.type = 'text/javascript';
+            wf.async = 'true';
+    
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(wf, s);
+        })();
+    }
 
     return App;
 

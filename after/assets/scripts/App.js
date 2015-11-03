3c95573fbfcb974139946083cb085f98949dd765
Floater <floater@Nerdery-OSX-2.local>
2015-11-02 21:16:19 -0600

    Adding before/after.

---
+++ after/assets/scripts/App.js
@@ -0,0 +1,29 @@
+define(function(require, exports, module) { // jshint ignore:line
+    'use strict';
+    
+
+    /**
+     * Initial application setup. Runs once upon every page load.
+     *
+     * @class App
+     * @constructor
+     */
+    var App = function() {
+        this.init();
+    };
+
+    var proto = App.prototype;
+
+    /**
+     * Initializes the application and kicks off loading of prerequisites.
+     *
+     * @method init
+     * @private
+     */
+    proto.init = function() {
+        // Create your views here
+    };
+
+    return App;
+
+});
